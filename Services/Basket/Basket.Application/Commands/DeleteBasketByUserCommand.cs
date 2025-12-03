901d071bce012e46e451dbd8a24898d40347ebd1
Hasanqp <hasangubran6@gmail.com>
2025-12-03 07:04:15 +0300

    implement DeleteBasketByUserName command and handler - Create DeleteBasketByUserNameCommand - Implement DeleteBasketByUserNameCommandHandler - Use Unit type for commands without return value - Complete CRUD operations for basket

---
+++ Services/Basket/Basket.Application/Commands/DeleteBasketByUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Basket.Application.Commands
+{
+    public class DeleteBasketByUserCommand : IRequest<Unit>
+    {
+        public string UserName { get; set; }
+        public DeleteBasketByUserCommand(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Application/Handlers/DeleteBasketByUserHandler.cs
@@ -0,0 +1,22 @@
+﻿using Basket.Application.Commands;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Handlers
+{
+    public class DeleteBasketByUserHandler : IRequestHandler<DeleteBasketByUserCommand, Unit>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+
+        public DeleteBasketByUserHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+        public async Task<Unit> Handle(DeleteBasketByUserCommand request, CancellationToken cancellationToken)
+        {
+            await _basketRepository.DeleteBasket(request.UserName);
+            return Unit.Value;
+        }
+    }
+}
