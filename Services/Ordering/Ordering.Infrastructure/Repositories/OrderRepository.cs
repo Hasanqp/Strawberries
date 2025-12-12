dbe8075e69d62b7d13d6e6636ec756c587fa4938
Hasanqp <hasangubran6@gmail.com>
2025-12-13 00:01:03 +0300

    create OrderRepository for custom order operations - Inherit from RepositoryBase<Order> - Implement IOrderRepository interface - Add GetOrdersByUserName method - Fix _dbContext access from private to protected

---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Ripositories;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        private readonly OrderContext _dbContext;
+
+        public OrderRepository(OrderContext dbContext): base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                .Where(o => o.UserName == userName)
+                .ToListAsync();
+            return orderList;
+        }
+
+    }
+}
