28bb157b69264514057f6b8481f0cb91ebcb1cf4
Hasanqp <hasangubran6@gmail.com>
2025-12-12 23:31:43 +0300

    implement OrderContextSeed for initial test data - Create SeedAsync method with conditional data insertion - Add sample orders for testing - Implement logging for seed operations

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed 
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if(!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded!!!");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "hasan",
+                    FirstName = "hasan",
+                    LastName = "ali",
+                    EmailAddress = "hasanali@Strawberries.net",
+                    AddressLine = "Moskovskaya Ulitsa 1, Kirov",
+                    Country = "Russia",
+                    TotalPrice = 750,
+                    State = "KIR",
+                    ZipCode = "610020",
+
+                    CardName = "Mir",
+                    CardNumber = "1098765432123456",
+                    CreatedBy = "Hasan",
+                    Expiration = "12/12",
+                    Cvv = "101",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Hasan",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
