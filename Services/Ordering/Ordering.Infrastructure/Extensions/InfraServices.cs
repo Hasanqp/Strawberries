f3f25dbd62af9b225fa0581e20cc42a42f53944b
Hasanqp <hasangubran6@gmail.com>
2025-12-13 01:34:26 +0300

    configure Infrastructure services in DI container - Create InfraServices extension method - Register DbContext with SQL Server connection string - Register generic IAsyncRepository<> and specific IOrderRepository

---
+++ Services/Ordering/Ordering.Infrastructure/Extensions/InfraServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Core.Ripositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure.Extensions
+{
+    public static class InfraServices
+    {
+        public static IServiceCollection AddInfaServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<OrderContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("OrderingConnectionString")));
+            serviceCollection.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            serviceCollection.AddScoped<IOrderRepository, OrderRepository>();
+            return serviceCollection;
+        }
+    }
+}
