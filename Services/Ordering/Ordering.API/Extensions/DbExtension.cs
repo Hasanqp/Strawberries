f6149f4f482aeb9aa94b6d1cf17a88df04f69b21
Hasanqp <hasangubran6@gmail.com>
2025-12-13 07:50:13 +0300

    implement DbExtension for automatic database migration - Add retry policy with exponential backoff using Polly - Integrate with OrderContextSeed for initial data - Support migration and seeding in one operation

---
+++ Services/Ordering/Ordering.API/Extensions/DbExtension.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace Ordering.API.Extensions
+{
+    public static class DbExtension
+    {
+        public static IHost MigrationDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder)
+            where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation($"Начата миграция базы данных: {typeof(TContext).Name}");
+                    // retry strategy
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                        retryCount: 5,
+                        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                        onRetry: (exception, span, count) =>
+                        {
+                            logger.LogError($"Повторная попытка из-за {exception} {span}");
+
+                        });
+                    retry.Execute(() => CallSeedr(seeder, context, services));
+                    logger.LogInformation($"Миграция завершена: {typeof(TContext).Name}");
+                }
+                catch ( Exception ex )
+                {
+                    logger.LogError(ex, $"Произошла ошибка при миграции базы данных: {typeof(TContext).Name}");
+                }
+            }
+            return host;
+        }
+
+        private static void CallSeedr<TContext>(Action<TContext, IServiceProvider> seeder, TContext? context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
