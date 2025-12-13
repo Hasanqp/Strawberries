e665391f53f2a5ddc6208a55131be2e6863b208c
Hasanqp <hasangubran6@gmail.com>
2025-12-13 04:55:28 +0300

    implement DeleteOrderCommandHandler for order deletion - Inject IOrderRepository and ILogger via constructor - Add validation for order existence before deletion - Implement logging for tracking delete operations

---
+++ Services/Ordering/Ordering.Application/Handlers/DeleteOrderCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Core.Entities;
+using Ordering.Core.Ripositories;
+
+namespace Ordering.Application.Handlers
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, Unit>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, ILogger logger)
+        {
+            _orderRepository = orderRepository;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToDelete == null)
+            {
+                throw new OrderNotFoundException(nameof(Order), request.Id);
+            }
+            await _orderRepository.DeleteAsync(orderToDelete);
+            _logger.LogInformation($"Заказ с идентификатором {request.Id} успешно удалён.");
+            return Unit.Value;
+        }
+    }
+}
