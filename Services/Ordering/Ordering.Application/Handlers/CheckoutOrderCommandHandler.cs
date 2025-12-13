d810e44d2ef9d7ec779f8f65a345f245978b3ff7
Hasanqp <hasangubran6@gmail.com>
2025-12-13 03:53:12 +0300

    implement CheckoutOrderCommandHandler for processing order creation - Inject IOrderRepository, IMapper, and ILogger via constructor - Use AutoMapper to convert CheckoutOrderCommand to Order entity - Add logging for tracking order creation operations - Return generated OrderId as response - Install Microsoft.Extensions.Logging (8.0.1)

---
+++ Services/Ordering/Ordering.Application/Handlers/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Core.Entities;
+using Ordering.Core.Ripositories;
+
+namespace Ordering.Application.Handlers
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var generatedOrder = await _orderRepository.AddAsync(orderEntity);
+            _logger.LogInformation($"Заказ с идентификатором {generatedOrder.Id} успешно создан");
+            return generatedOrder.Id;
+        }
+    }
+}
