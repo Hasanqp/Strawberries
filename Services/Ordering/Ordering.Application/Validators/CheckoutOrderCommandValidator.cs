fc5a226148dc179607a0f20e2ccac7bf76f6466f
Hasanqp <hasangubran6@gmail.com>
2025-12-13 05:35:54 +0300

    implement FluentValidation for data validation - Install FluentValidation.DependencyInjectionExtensions - Create CheckoutOrderCommandValidator with validation rules - Add validation for UserName, TotalPrice, Email, FirstName, LastName

---
+++ Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(o => o.UserName)
+                .NotEmpty()
+                .WithMessage("{UserName} обязательно для заполнения.")
+                .NotNull()
+                .MaximumLength(70)
+                .WithMessage("{UserName} не должно превышать 70 символов.");
+            RuleFor(o => o.TotalPrice)
+                .NotEmpty()
+                .WithMessage("{TotalPrice} обязательно для заполнения.")
+                .GreaterThan(-1)
+                .WithMessage("{TotalPrice} не должно быть отрицательным.");
+            RuleFor(o => o.EmailAddress)
+                .NotEmpty()
+                .WithMessage("{EmailAddress} обязательно для заполнения.");
+            RuleFor(o => o.FirstName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{FirstName} обязательно для заполнения.");
+            RuleFor(o => o.LastName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("{LastName} обязательно для заполнения.");
+        }
+    }
+}
