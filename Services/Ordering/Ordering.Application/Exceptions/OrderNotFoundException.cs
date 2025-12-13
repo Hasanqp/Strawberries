015c3827ca50023a0a6c406f0094ee7a94d27b31
Hasanqp <hasangubran6@gmail.com>
2025-12-13 04:57:02 +0300

    create custom OrderNotFoundException for better error handling - Extend Exception class with custom constructor - Include entity name and key in error message - Improve debugging and user experience

---
+++ Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundException : ApplicationException
+    {
+        public OrderNotFoundException(string name, object key) : base($"Сущность {name} с идентификатором {key} не найдена.")
+        {
+
+        }
+    }
+}
