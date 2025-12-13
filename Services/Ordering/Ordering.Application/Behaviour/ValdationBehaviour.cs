9433c6e9a885b1c9ea89df065a0fcc930b3806db
Hasanqp <hasangubran6@gmail.com>
2025-12-13 06:20:10 +0300

    implement ValidationPipelineBehavior for auto-validation - Implement IPipelineBehavior interface - Run all validators in parallel for performance - Throw ValidationException on validation failure

---
+++ Services/Ordering/Ordering.Application/Behaviour/ValdationBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviour
+{
+    // This will collect fluent validators and rn before handler
+    public class ValdationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValdationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                // This will run all the validation rules one by one nd returns the falidation result
+                var validationResults = await Task.WhenAll(
+                    _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                // Now need to check for any failure
+                var failures = validationResults.SelectMany(e => e.Errors).Where(f => f != null).ToList();
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+            // On Success case
+            return await next();
+        }
+    }
+}
