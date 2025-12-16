40126703657efc6cf5f99c41162b50b963d032ee
Hasanqp <hasangubran6@gmail.com>
2025-12-16 05:27:56 +0300

    define BasketCheckoutEvent and related classes - Create BaseIntegrationEvent with CorrelationId and CreationDate - Implement BasketCheckoutEvent for checkout messaging - Add EventBusConstants for shared queue names

---
+++ Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class BaseIntegrationEvent
+    {
+        public string CorelationId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public BaseIntegrationEvent()
+        {
+            CorelationId = Guid.NewGuid().ToString();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public BaseIntegrationEvent(Guid corelationId, DateTime creationDate)
+        {
+            CorelationId = corelationId.ToString();
+            CreationDate = creationDate;
+        }
+    }
+}
