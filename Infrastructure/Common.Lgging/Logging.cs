1ef2409b1e805e1f15d8212615f3312146c18b43
Hasanqp <hasangubran6@gmail.com>
2025-12-19 02:00:19 +0300

    feat: implement static Logging class with ConfigureLogger - Create Logging.cs with ConfigureLogger static property - Implement log level filtering (Information, Debug, etc.) - Add Microsoft.AspNetCore and Microsoft.Hosting.Lifetime overrides - Configure service-specific debug levels (Catalog, Basket, Discount, Ordering)

---
+++ Infrastructure/Common.Lgging/Logging.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace Common.Lgging
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> cfg =>
+            (context, LoggerConfiguration) =>
+            {
+                var env = context.HostingEnvironment;
+                LoggerConfiguration.MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                .Enrich.WithExceptionDetails()
+                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspHosting.Lifetime", Serilog.Events.LogEventLevel.Warning)
+                .WriteTo.Console();
+
+                if(context.HostingEnvironment.IsDevelopment())
+                {
+                    LoggerConfiguration.MinimumLevel.Override("Catalog", Serilog.Events.LogEventLevel.Debug);
+                    LoggerConfiguration.MinimumLevel.Override("Basket", Serilog.Events.LogEventLevel.Debug);
+                    LoggerConfiguration.MinimumLevel.Override("Discount", Serilog.Events.LogEventLevel.Debug);
+                    LoggerConfiguration.MinimumLevel.Override("Ordering", Serilog.Events.LogEventLevel.Debug);
+                }
+            };
+    }
+}
