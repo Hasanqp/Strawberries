07d0f09b8e5b34f24dc797f623b46bf20a812ba9
Hasanqp <hasangubran6@gmail.com>
2025-12-20 20:38:39 +0300

    feat: create BasketCheckoutEventV2 for event-driven architecture - Implement BasketCheckoutEventV2 inheriting from IntegrationEvent - Simplify event structure to match V2 model - Prepare for RabbitMQ/Event Bus integration - Create separate folder structure for V2 events

---
+++ Services/Basket/Basket.API/Controllers/V2/BasketController.cs
@@ -0,0 +1,53 @@
+﻿using Asp.Versioning;
+using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Basket.Application.Queries;
+using Basket.Core.Entities;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers.V2
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketController> _logger;
+        public BasketController(IMediator mediator, IPublishEndpoint publishEndpoint, ILogger<BasketController> logger)
+        {
+            _mediator = mediator;
+            _publishEndpoint = publishEndpoint;
+            _logger = logger;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // Getthe Existing basket with username
+            var query = new GetBasketByUserNameQuery(basketCheckout.UserName);
+            var basket = await _mediator.Send(query);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            var eventMsg = BasketMapper.Mapper.Map<BasketCheckoutEventv2>(basketCheckout);
+            eventMsg.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMsg);
+            _logger.LogInformation($"Basket Published for {basket.UserName} with V2 endpoint");
+            // remove the basket
+            var deleteCmd = new DeleteBasketByUserNameCommand(basketCheckout.UserName);
+            await _mediator.Send(deleteCmd);
+            return Accepted();
+        }
+    }
+}
